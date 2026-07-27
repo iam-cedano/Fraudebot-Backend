f43215c3eb585562ea88c78659694da5079bc62b
Oscar Cedano <oscar.cedano@outlook.com>
2026-07-27 00:46:30 -0600

    ✅ feat: adding Report to Seeeder

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Contact;
 use App\Models\Organization;
+use App\Models\Report;
 use App\Models\Scammer;
 use Illuminate\Database\Seeder;
 
@@ -14,16 +15,19 @@ class TestSeeder extends Seeder
         $scammers = (int) env('SEED_SCAMMERS', 1);
         $contacts = (int) env('SEED_CONTACTS', 3);
         $organizations = (int) env('SEED_ORGANIZATIONS', 1);
+        $reports = (int) env('SEED_REPORTS', 2);
 
         Scammer::factory()
             ->count($scammers)
             ->has(Organization::factory())
             ->has(Contact::factory()->count($contacts)->state(['organization_id' => null]))
+            ->has(Report::factory()->count($reports)->state(['organization_id' => null]))
             ->create();
 
         Organization::factory()
             ->count($organizations)
             ->has(Contact::factory()->count($contacts)->state(['scammer_id' => null]))
+            ->has(Report::factory()->count($reports)->state(['scammer_id' => null]))
             ->create();
     }
 }
