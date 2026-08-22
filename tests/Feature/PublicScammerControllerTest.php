c9bea3b5b68ae54f07c8a3f22bf5e7426721af41
Oscar Cedano <oscar.cedano@outlook.com>
2026-08-22 14:39:56 -0600

    ✅ feat: Public/ScammerController::contacts() test

--- tests/Feature/PublicScammerControllerTest.php
+++ tests/Feature/PublicScammerControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Http\Resources\Public\ContactResource;
+use App\Models\Contact;
 use App\Models\Scammer;
 use App\Http\Resources\Public\ScammerResource;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -36,5 +38,30 @@ class PublicScammerControllerTest extends TestCase
 
         $response->assertStatus(404);
         $response->assertExactJson(['message' => 'Scammer not found']);
+    }
+
+
+    public function testFindContactsByScammerId(): void
+    {
+        /**
+         * @var Scammer $scammer
+         */
+        $scammer = Scammer::factory()->create();
+        $contact = Contact::factory()->create();
+
+        $scammer->contacts()->attach($contact->id);
+
+        $page = 1;
+        $count = 10;
+
+        $response = $this->getJson("/api/public/scammers/{$scammer->id}/contacts?p={$page}");
+
+        $response->assertStatus(200);
+        $response->assertExactJson([
+            'data' => ContactResource::collection($scammer->contacts)->resolve(),
+            'total' => $scammer->contacts->count(),
+            'page' => $page,
+            'count' => $count,
+        ]);
     }
 }
