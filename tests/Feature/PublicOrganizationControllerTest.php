091f3ed1fc10dfda9e8568ea03d37ed21a3822d3
Oscar Cedano <oscar.cedano@outlook.com>
2026-08-22 22:11:56 -0600

    ✅ feat: updating test suite for Public/OrganizationController

--- tests/Feature/PublicOrganizationControllerTest.php
+++ tests/Feature/PublicOrganizationControllerTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Domain\Contact\Enums\PlatformType;
+use App\Http\Resources\Public\ContactResource;
 use App\Http\Resources\Public\OrganizationResource;
+use App\Models\Contact;
 use App\Models\Organization;
 use App\Models\Report;
 use Tests\TestCase;
@@ -83,5 +86,113 @@ class PublicOrganizationControllerTest extends TestCase
 
         $response->assertStatus(404);
         $response->assertExactJson(['message' => 'Organization not found']);
+    }
+
+    public function testFindOrganizationContactsById(): void
+    {
+        $organization = Organization::factory()->create();
+
+        $contacts = Contact::factory()->count(3)->create();
+
+        $organization->contacts()->attach($contacts->pluck('id'));
+
+        $page = 1;
+        $count = 10;
+        $expected = [
+            'data' => ContactResource::collection($contacts)->resolve(),
+            'total' => $contacts->count(),
+            'page' => $page,
+            'count' => $count,
+        ];
+
+        $response = $this->getJson("/api/public/organizations/{$organization->id}/contacts?p={$page}&c={$count}");
+
+        $response->assertStatus(200);
+        $response->assertExactJson($expected);
+    }
+
+    public function testFindoOrganizationContactsByIdWithPlatformQueryParam(): void
+    {
+        $organization = Organization::factory()->create();
+        $contacts = Contact::factory()->createMany([
+            [
+                'name' => 'John Doe',
+                'reference' => 'john-doe',
+                'platform' => PlatformType::INSTAGRAM,
+                'is_active' => true,
+            ],
+            [
+                'name' => 'Jane Doe',
+                'reference' => 'jane-doe',
+                'platform' => PlatformType::FACEBOOK,
+                'is_active' => true,
+            ],
+            [
+                'name' => 'Jim Doe',
+                'reference' => 'jim-doe',
+                'platform' => PlatformType::TELEGRAM,
+                'is_active' => true,
+            ],
+        ]);
+        $organization->contacts()->attach($contacts->pluck('id'));
+
+        $page = 1;
+        $count = 10;
+        $platform = 'instagram';
+        $expected = [
+            'data' => ContactResource::collection($contacts->where('platform', PlatformType::INSTAGRAM))->resolve(),
+            'total' => $contacts->where('platform', PlatformType::INSTAGRAM)->count(),
+            'page' => $page,
+            'count' => $count,
+        ];
+
+        $response = $this->getJson("/api/public/organizations/{$organization->id}/contacts?p={$page}&c={$count}&platform={$platform}");
+
+        $response->assertStatus(200);
+        $response->assertExactJson($expected);
+    }
+
+    public function testFindOrganizationContactsByIdWithInvalidPageReturns404(): void
+    {
+        $page = 1;
+        $count = 10;
+
+        $response = $this->getJson("/api/public/organizations/1/contacts?p={$page}&c={$count}");
+
+        $response->assertStatus(404);
+        $response->assertExactJson(['message' => 'Organization not found']);
+    }
+
+    public function testFindOrganizationContactsByIdWithInvalidPageQueryParamReturns400(): void
+    {
+        $page = 'invalid-page';
+        $count = 10;
+
+        $response = $this->getJson("/api/public/organizations/1/contacts?p={$page}&c={$count}");
+
+        $response->assertStatus(400);
+        $response->assertExactJson(['message' => 'Invalid page']);
+    }
+
+    public function testFindOrganizationContactsByIdWithInvalidCountQueryParamReturns400(): void
+    {
+        $page = 1;
+        $count = 'invalid-count';
+
+        $response = $this->getJson("/api/public/organizations/1/contacts?p={$page}&c={$count}");
+
+        $response->assertStatus(400);
+        $response->assertExactJson(['message' => 'Invalid count']);
+    }
+
+    public function testFindOrganizationContactsByIdWithInvalidOrganizationIdParamReturns400(): void
+    {
+        $page = 1;
+        $count = 10;
+
+        $response = $this->getJson("/api/public/organizations/invalid-organization-id/contacts?p={$page}&c={$count}");
+
+        $response->assertStatus(400);
+        $response->assertExactJson(['message' => 'Invalid organization ID']);
     }
 }
